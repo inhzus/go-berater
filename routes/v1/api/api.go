b37de91edea8561f4653eddd30c4d4d4b4055004
Zhi Sun <inhzus@gmail.com>
2019-05-30 22:22:54 +0800

    [Fix] Jwt package location

--- routes/v1/api/api.go
+++ routes/v1/api/api.go
@@ -2,7 +2,7 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/inhzus/go-berater/middlewares/jwt"
+	"github.com/inhzus/go-berater/middlewares"
 	"net/http"
 )
 
@@ -12,7 +12,7 @@ func ApplyRoutes(r *gin.RouterGroup) {
 		api.GET("/test/token/:openid", testToken)
 	}
 	auth := api.Group("")
-	auth.Use(jwt.JwtMiddleware())
+	auth.Use(middlewares.JwtMiddleware())
 	{
 		auth.GET("/token", checkToken)
 	}
@@ -20,7 +20,7 @@ func ApplyRoutes(r *gin.RouterGroup) {
 
 func testToken(c *gin.Context) {
 	openid := c.Param("openid")
-	auth, err := jwt.CreateToken(openid)
+	auth, err := middlewares.CreateToken(openid)
 	if err != nil {
 		_ = c.AbortWithError(http.StatusBadRequest, err)
 		return
